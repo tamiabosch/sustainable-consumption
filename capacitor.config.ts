42b6baa861fd1f334b4e4e01830b2313ac25880a
Tamia Bosch <tamia@bizfactory.tech>
2022-04-11 21:25:40 +0200

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sustainable-consumption',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
